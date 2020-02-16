a37289e08279aa9f0622c58cde54f1ffd0f048fa
Lukasz Jader <ljader@esri.pl>
2020-02-16 17:13:11 +0100

    Add failing junit test

---
+++ src/test/java/pl/ljader/demo/demowebappforjenkins/DateTimeProviderTest.java
@@ -0,0 +1,19 @@
+package pl.ljader.demo.demowebappforjenkins;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DateTimeProviderTest {
+
+	@Test
+	final void testGetDate() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	final void testGetDateTime() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
