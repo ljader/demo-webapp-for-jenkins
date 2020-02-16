d4decfb9e485788e39696769c6ce7472701c9a14
Lukasz Jader <ljader@esri.pl>
2020-02-16 13:57:30 +0100

    Fresh web project from start.spring.io

---
+++ src/main/java/pl/ljader/demo/demowebappforjenkins/DemoWebappForJenkinsApplication.java
@@ -0,0 +1,13 @@
+package pl.ljader.demo.demowebappforjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWebappForJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoWebappForJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/ljader/demo/demowebappforjenkins/DemoWebappForJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.ljader.demo.demowebappforjenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoWebappForJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
